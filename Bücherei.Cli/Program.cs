be4a0388d60f1e297ef2be60492f05b9424bcf7e
benny <baeni.saa@gmail.com>
2024-06-04 12:20:58 +0200

    Adjustments to commands, drop all tables on re-run program

--- Bücherei.Cli/Program.cs
+++ Bücherei.Cli/Program.cs
@@ -1,26 +1,37 @@
 ﻿using Npgsql;
 
 var connString = "Host=localhost;Port=54321;Database=postgres;Username=postgres;Password=password123";
-var conn = new NpgsqlConnection(connString);
+var dataSource = NpgsqlDataSource.Create(connString);
 
-conn.Open();
 
-// Autoren
-var autorenCreateCmd = new NpgsqlCommand("CREATE TABLE IF NOT EXISTS autoren (id SERIAL PRIMARY KEY, vorname varchar, nachname varchar);", conn);
-var autorenInsertCmd = new NpgsqlCommand("INSERT INTO autoren (vorname, nachname) VALUES ('John', 'Doe')", conn);
-if (autorenCreateCmd.ExecuteNonQuery() > 0)
-    autorenInsertCmd.ExecuteNonQuery();
 
-// Büchereien
-var büchereienCreateCmd = new NpgsqlCommand("CREATE TABLE IF NOT EXISTS büchereien (id SERIAL PRIMARY KEY, name varchar);", conn);
-var büchereienInsertCmd = new NpgsqlCommand("INSERT INTO büchereien (name) VALUES ('Bibel')", conn);
-if (büchereienCreateCmd.ExecuteNonQuery() > 0)
-    büchereienInsertCmd.ExecuteNonQuery();
+// Drop all tables
+await using var dropTablesCmd = dataSource.CreateCommand("DROP TABLE IF EXISTS bücher, büchereien, autoren");
+await dropTablesCmd.ExecuteNonQueryAsync();
 
-// Bücher
-var bücherCreateCmd = new NpgsqlCommand("CREATE TABLE IF NOT EXISTS bücher (id SERIAL PRIMARY KEY, büchereiId int REFERENCES büchereien(id), titel varchar, autorId int REFERENCES autoren(id));", conn);
-var bücherInsertCmd = new NpgsqlCommand("INSERT INTO bücher (büchereiId, titel, autorId) VALUES (1, 'Stadtbücherei Lorem', 1)", conn);
-if (bücherCreateCmd.ExecuteNonQuery() > 0)
-    bücherInsertCmd.ExecuteNonQuery();
 
-conn.Close();
+
+// Create table Autoren
+await using var autorenCreateCmd = dataSource.CreateCommand("CREATE TABLE IF NOT EXISTS autoren (id SERIAL PRIMARY KEY, vorname varchar, nachname varchar);");
+await autorenCreateCmd.ExecuteNonQueryAsync();
+// Insert into table Autoren
+await using var autorenInsertCmd = dataSource.CreateCommand("INSERT INTO autoren (vorname, nachname) VALUES ('John', 'Doe')");
+await autorenInsertCmd.ExecuteNonQueryAsync();
+
+
+
+// Create table Büchereien
+await using var büchereienCreateCmd = dataSource.CreateCommand("CREATE TABLE IF NOT EXISTS büchereien (id SERIAL PRIMARY KEY, name varchar);");
+await büchereienCreateCmd.ExecuteNonQueryAsync();
+// Insert into table Büchereien
+await using var büchereienInsertCmd = dataSource.CreateCommand("INSERT INTO büchereien (name) VALUES ('Bibel')");
+await büchereienInsertCmd.ExecuteNonQueryAsync();
+
+
+
+// Create table Bücher
+await using var bücherCreateCmd = dataSource.CreateCommand("CREATE TABLE IF NOT EXISTS bücher (id SERIAL PRIMARY KEY, büchereiId int REFERENCES büchereien(id), titel varchar, autorId int REFERENCES autoren(id));");
+await bücherCreateCmd.ExecuteNonQueryAsync();
+// Insert into table Bücher
+await using var bücherInsertCmd = dataSource.CreateCommand("INSERT INTO bücher (büchereiId, titel, autorId) VALUES (1, 'Stadtbücherei Lorem', 1)");
+await bücherInsertCmd.ExecuteNonQueryAsync();
