2fd28f409887a127f4279896167fef91f664808c
benny <baeni.saa@gmail.com>
2024-06-04 13:22:44 +0200

    Creat table buchobjekte and insert sample object as json

--- Bücherei.Cli/Program.cs
+++ Bücherei.Cli/Program.cs
@@ -1,4 +1,6 @@
-﻿using Npgsql;
+﻿using System.Text.Json;
+using Bücherei.Lib.Entities;
+using Npgsql;
 
 var connString = "Host=localhost;Port=54321;Database=postgres;Username=postgres;Password=password123";
 var dataSource = NpgsqlDataSource.Create(connString);
@@ -6,7 +8,7 @@ var dataSource = NpgsqlDataSource.Create(connString);
 
 
 // Drop all tables
-await using var dropTablesCmd = dataSource.CreateCommand("DROP TABLE IF EXISTS bücher, büchereien, autoren");
+await using var dropTablesCmd = dataSource.CreateCommand("DROP TABLE IF EXISTS bücher, büchereien, autoren, buchobjekte");
 await dropTablesCmd.ExecuteNonQueryAsync();
 
 
@@ -37,13 +39,32 @@ await büchereienInsertCmd.ExecuteNonQueryAsync();
 
 
 // Create table Bücher
-await using var bücherCreateCmd = dataSource.CreateCommand("CREATE TABLE IF NOT EXISTS bücher (id SERIAL PRIMARY KEY, büchereiId int REFERENCES büchereien(id), titel varchar, autorId int REFERENCES autoren(id));");
+await using var bücherCreateCmd = dataSource.CreateCommand("CREATE TABLE IF NOT EXISTS bücher (id SERIAL PRIMARY KEY, titel varchar, autorId int REFERENCES autoren(id), büchereiId int REFERENCES büchereien(id));");
 await bücherCreateCmd.ExecuteNonQueryAsync();
 // Insert into table Bücher
-await using var bücherInsertCmd = dataSource.CreateCommand("INSERT INTO bücher (büchereiId, titel, autorId) VALUES " +
-                                                           "(1, 'Max und Moritz', 1), " +
-                                                           "(1, 'Der Wüstenplanet', 2), " +
-                                                           "(2, 'Die Bibel', 3), " +
-                                                           "(1, 'Pipi Langstrumpf', 4), " +
-                                                           "(2, 'Die Stadt der Träumenden Bücher', 5)");
+await using var bücherInsertCmd = dataSource.CreateCommand("INSERT INTO bücher (titel, autorId, büchereiId) VALUES " +
+                                                           "('Max und Moritz', 1, 1), " +
+                                                           "('Der Wüstenplanet', 2, 1), " +
+                                                           "('Die Bibel', 3, 2), " +
+                                                           "('Pipi Langstrumpf', 4, 1), " +
+                                                           "('Die Stadt der Träumenden Bücher', 5, 2)");
 await bücherInsertCmd.ExecuteNonQueryAsync();
+
+
+
+// Create table Buchobjekte
+await using var buchobjekteCreateCmd = dataSource.CreateCommand("CREATE TABLE IF NOT EXISTS buchobjekte (id SERIAL PRIMARY KEY, buchobjekt JSONB);");
+await buchobjekteCreateCmd.ExecuteNonQueryAsync();
+// Insert into table Buchobjekte
+var autor = new Autor { Id = 1, Vorname = "Wilhelm", Nachname = "Busch" };
+var bücherei = new Bücherei.Lib.Entities.Bücherei { Id = 1, Name = "Staatsbibliothek zu Berlin" };
+var buch = new Buch
+{
+    Id = 1,
+    Titel = "Max und Moritz",
+    Autor = autor,
+    Bücherei = bücherei
+};
+await using var buchobjekteInsertCmd = dataSource.CreateCommand("INSERT INTO buchobjekte (buchobjekt) VALUES " +
+                                                           $"('{JsonSerializer.Serialize(buch)}')");
+await buchobjekteInsertCmd.ExecuteNonQueryAsync();
