06645783144cdf3f96017c191fa2716a907b241f
benny <baeni.saa@gmail.com>
2024-06-04 12:37:00 +0200

    Add sample data

--- Bücherei.Cli/Program.cs
+++ Bücherei.Cli/Program.cs
@@ -15,7 +15,12 @@ await dropTablesCmd.ExecuteNonQueryAsync();
 await using var autorenCreateCmd = dataSource.CreateCommand("CREATE TABLE IF NOT EXISTS autoren (id SERIAL PRIMARY KEY, vorname varchar, nachname varchar);");
 await autorenCreateCmd.ExecuteNonQueryAsync();
 // Insert into table Autoren
-await using var autorenInsertCmd = dataSource.CreateCommand("INSERT INTO autoren (vorname, nachname) VALUES ('John', 'Doe')");
+await using var autorenInsertCmd = dataSource.CreateCommand("INSERT INTO autoren (vorname, nachname) VALUES " +
+                                                            "('Wilhelm', 'Busch'), " +
+                                                            "('Frank', 'Herbert'), " +
+                                                            "('Unbekannt', 'Unbekannt'), " +
+                                                            "('Astrid', 'Lindgren'), " +
+                                                            "('Walter', 'Moers')");
 await autorenInsertCmd.ExecuteNonQueryAsync();
 
 
@@ -24,7 +29,9 @@ await autorenInsertCmd.ExecuteNonQueryAsync();
 await using var büchereienCreateCmd = dataSource.CreateCommand("CREATE TABLE IF NOT EXISTS büchereien (id SERIAL PRIMARY KEY, name varchar);");
 await büchereienCreateCmd.ExecuteNonQueryAsync();
 // Insert into table Büchereien
-await using var büchereienInsertCmd = dataSource.CreateCommand("INSERT INTO büchereien (name) VALUES ('Bibel')");
+await using var büchereienInsertCmd = dataSource.CreateCommand("INSERT INTO büchereien (name) VALUES " +
+                                                               "('Staatsbibliothek zu Berlin'), " +
+                                                               "('Universitäts Johann Christian Senckenberg')");
 await büchereienInsertCmd.ExecuteNonQueryAsync();
 
 
@@ -33,5 +40,10 @@ await büchereienInsertCmd.ExecuteNonQueryAsync();
 await using var bücherCreateCmd = dataSource.CreateCommand("CREATE TABLE IF NOT EXISTS bücher (id SERIAL PRIMARY KEY, büchereiId int REFERENCES büchereien(id), titel varchar, autorId int REFERENCES autoren(id));");
 await bücherCreateCmd.ExecuteNonQueryAsync();
 // Insert into table Bücher
-await using var bücherInsertCmd = dataSource.CreateCommand("INSERT INTO bücher (büchereiId, titel, autorId) VALUES (1, 'Stadtbücherei Lorem', 1)");
+await using var bücherInsertCmd = dataSource.CreateCommand("INSERT INTO bücher (büchereiId, titel, autorId) VALUES " +
+                                                           "(1, 'Max und Moritz', 1), " +
+                                                           "(1, 'Der Wüstenplanet', 2), " +
+                                                           "(2, 'Die Bibel', 3), " +
+                                                           "(1, 'Pipi Langstrumpf', 4), " +
+                                                           "(2, 'Die Stadt der Träumenden Bücher', 5)");
 await bücherInsertCmd.ExecuteNonQueryAsync();
