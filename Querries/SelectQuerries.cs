c9ad9c4a19e70f8881ec47c904db65a63d37a226
maubach <maubach@DE-ADN-3CWB2T3>
2024-06-05 12:39:47 +0200

    großes setup script ersetzt durch SampleDataService.

--- Querries/SelectQuerries.cs
+++ Querries/SelectQuerries.cs
@@ -1,10 +1,6 @@
-using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
-using System.Text.Json;
-using Bücherei.Lib.Entities;
-using Microsoft.Extensions.Logging;
+using Bücherei.Lib.Services;
 using Npgsql;
+using System.Diagnostics;
 using Xunit.Abstractions;
 
 namespace Querries
@@ -27,64 +23,12 @@ namespace Querries
         {
             DataSource = NpgsqlDataSource.Create(ConnectionString);
 
-            // Drop all tables
-            await using var dropTablesCmd = DataSource.CreateCommand("DROP TABLE IF EXISTS bücher, büchereien, autoren, buchobjekte");
-            await dropTablesCmd.ExecuteNonQueryAsync();
+            SampleDataService sampleDataService = new SampleDataService(
+                connectionString: "Host=localhost;Port=54321;Database=postgres;Username=postgres;Password=password123",
+                sampleDataJsonPath: "../../../../Bücherei.Cli/sample-data.json");
 
-
-            // Create table Autoren
-            await using var autorenCreateCmd = DataSource.CreateCommand("CREATE TABLE IF NOT EXISTS autoren (id SERIAL PRIMARY KEY, vorname varchar, nachname varchar);");
-            await autorenCreateCmd.ExecuteNonQueryAsync();
-            // Insert into table Autoren
-            await using var autorenInsertCmd = DataSource.CreateCommand("INSERT INTO autoren (vorname, nachname) VALUES " +
-                                                                        "('Wilhelm', 'Busch'), " +
-                                                                        "('Frank', 'Herbert'), " +
-                                                                        "('Unbekannt', 'Unbekannt'), " +
-                                                                        "('Astrid', 'Lindgren'), " +
-                                                                        "('Walter', 'Moers')");
-            await autorenInsertCmd.ExecuteNonQueryAsync();
-
-
-            // Create table Büchereien
-            await using var büchereienCreateCmd = DataSource.CreateCommand("CREATE TABLE IF NOT EXISTS büchereien (id SERIAL PRIMARY KEY, name varchar);");
-            await büchereienCreateCmd.ExecuteNonQueryAsync();
-            // Insert into table Büchereien
-            await using var büchereienInsertCmd = DataSource.CreateCommand("INSERT INTO büchereien (name) VALUES " +
-                                                                           "('Staatsbibliothek zu Berlin'), " +
-                                                                           "('Universitäts Johann Christian Senckenberg')");
-            await büchereienInsertCmd.ExecuteNonQueryAsync();
-
-
-            // Create table Bücher
-            await using var bücherCreateCmd = DataSource.CreateCommand("CREATE TABLE IF NOT EXISTS bücher (id SERIAL PRIMARY KEY, titel varchar, autorId int REFERENCES autoren(id), büchereiId int REFERENCES büchereien(id));");
-            await bücherCreateCmd.ExecuteNonQueryAsync();
-            // Insert into table Bücher
-            await using var bücherInsertCmd = DataSource.CreateCommand("INSERT INTO bücher (titel, autorId, büchereiId) VALUES " +
-                                                                       "('Max und Moritz', 1, 1), " +
-                                                                       "('Der Wüstenplanet', 2, 1), " +
-                                                                       "('Die Bibel', 3, 2), " +
-                                                                       "('Pipi Langstrumpf', 4, 1), " +
-                                                                       "('Die Stadt der Träumenden Bücher', 5, 2)");
-            await bücherInsertCmd.ExecuteNonQueryAsync();
-
-
-
-            // Create table Buchobjekte
-            await using var buchobjekteCreateCmd = DataSource.CreateCommand("CREATE TABLE IF NOT EXISTS buchobjekte (id SERIAL PRIMARY KEY, buchobjekt JSONB);");
-            await buchobjekteCreateCmd.ExecuteNonQueryAsync();
-            // Insert into table Buchobjekte
-            var autor = new Autor { Id = 1, Vorname = "Wilhelm", Nachname = "Busch" };
-            var bücherei = new Bücherei.Lib.Entities.Bücherei { Id = 1, Name = "Staatsbibliothek zu Berlin" };
-            var buch = new Buch
-            {
-                Id = 1,
-                Titel = "Max und Moritz",
-                Autor = autor,
-                Bücherei = bücherei
-            };
-            await using var buchobjekteInsertCmd = DataSource.CreateCommand("INSERT INTO buchobjekte (buchobjekt) VALUES " +
-                                                                       $"('{JsonSerializer.Serialize(buch)}')");
-            await buchobjekteInsertCmd.ExecuteNonQueryAsync();
+            await sampleDataService.RecreateTablesAsync();
+            await sampleDataService.FillTablesWithSampleDataAsync();
         }
 
         //X
