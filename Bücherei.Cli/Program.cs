6217b6465ca7ba62d64c72633c23700a26b0aec0
benny <baeni.saa@gmail.com>
2024-06-05 11:58:11 +0200

    Implement SampleDataService to outsource logic out of Cli project

--- Bücherei.Cli/Program.cs
+++ Bücherei.Cli/Program.cs
@@ -1,50 +1,8 @@
-﻿using System.Text.Json;
-using Bücherei.Cli;
-using Npgsql;
+﻿using Bücherei.Lib.Services;
 
-var connString = "Host=localhost;Port=54321;Database=postgres;Username=postgres;Password=password123";
-var dataSource = NpgsqlDataSource.Create(connString);
+SampleDataService sampleDataService = new SampleDataService(
+    connectionString: "Host=localhost;Port=54321;Database=postgres;Username=postgres;Password=password123",
+    sampleDataJsonPath: "./sample-data.json");
 
-var json = await File.ReadAllTextAsync("./sample-data.json");
-var data = JsonSerializer.Deserialize<Data>(json);
-
-// Delete tables
-await using var dropTablesCmd = dataSource.CreateCommand("DROP TABLE IF EXISTS bücher, büchereien, autoren, buchobjekte");
-await dropTablesCmd.ExecuteNonQueryAsync();
-
-// Create tables
-await using var createTablesCmd = dataSource.CreateCommand(
-    "CREATE TABLE IF NOT EXISTS autoren (id SERIAL PRIMARY KEY, vorname varchar, nachname varchar);" +
-    "CREATE TABLE IF NOT EXISTS büchereien (id SERIAL PRIMARY KEY, name varchar);" +
-    "CREATE TABLE IF NOT EXISTS bücher (id SERIAL PRIMARY KEY, titel varchar, autorId int REFERENCES autoren(id), büchereiId int REFERENCES büchereien(id));");
-await createTablesCmd.ExecuteNonQueryAsync();
-
-// Insert into autoren
-foreach (var autor in data.Autoren)
-{
-    await using var cmd = dataSource.CreateCommand("INSERT INTO autoren (id, vorname, nachname) VALUES (@id, @vorname, @nachname)");
-    cmd.Parameters.AddWithValue("id", autor.Id);
-    cmd.Parameters.AddWithValue("vorname", autor.Vorname);
-    cmd.Parameters.AddWithValue("nachname", autor.Nachname);
-    await cmd.ExecuteNonQueryAsync();
-}
-
-// Insert into büchereien
-foreach (var bücherei in data.Büchereien)
-{
-    await using var cmd = dataSource.CreateCommand("INSERT INTO büchereien (id, name) VALUES (@id, @name)");
-    cmd.Parameters.AddWithValue("id", bücherei.Id);
-    cmd.Parameters.AddWithValue("name", bücherei.Name);
-    await cmd.ExecuteNonQueryAsync();
-}
-
-// Insert into bücher
-foreach (var buch in data.Bücher)
-{
-    await using var cmd = dataSource.CreateCommand("INSERT INTO bücher (id, titel, autorId, büchereiId) VALUES (@id, @titel, @autorId, @büchereiId)");
-    cmd.Parameters.AddWithValue("id", buch.Id);
-    cmd.Parameters.AddWithValue("titel", buch.Titel);
-    cmd.Parameters.AddWithValue("autorId", buch.AutorId);
-    cmd.Parameters.AddWithValue("büchereiId", buch.BüchereiId);
-    await cmd.ExecuteNonQueryAsync();
-}
+await sampleDataService.RecreateTablesAsync();
+await sampleDataService.FillTablesWithSampleDataAsync();
---
+++ Bücherei.Lib/Entities/SampleData.cs
@@ -0,0 +1,12 @@
+namespace Bücherei.Lib.Entities;
+
+public class SampleData
+{
+    public SampleData() {}
+
+    public List<Autor> Autoren { get; init; } = [];
+    
+    public List<Bücherei> Büchereien { get; init; } = [];
+
+    public List<Buch> Bücher { get; init; } = [];
+}
